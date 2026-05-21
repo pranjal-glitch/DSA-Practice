1351efb2bfa83e36d10f596a93f1fe5debd08add
pranjal-glitch <pranjal29240705@gmail.com>
2026-05-21 23:20:50 +0530

    Implement flatten function for binary tree

---
+++ BinaryTree/flatten.cpp
@@ -0,0 +1,19 @@
+void flattenBinaryTree(TreeNode<int>* root)
+{
+    // Write your code here.
+    TreeNode<int>* curr = root;
+
+    while(curr != NULL){
+        if(curr -> left != NULL){
+            TreeNode<int>* pred = curr -> left;
+            while(pred -> right != NULL){
+                pred = pred -> right;
+            }
+            pred -> right = curr -> right;
+
+            curr -> right = curr -> left;
+            curr -> left = NULL;
+        }
+        curr = curr -> right;
+    }
+}
