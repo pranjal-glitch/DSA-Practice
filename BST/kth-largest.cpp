c345275395d37a678a81df60902f4d4bf5f45845
pranjal-glitch <pranjal29240705@gmail.com>
2026-05-29 23:43:37 +0530

    Implement Kth largest number finder in BST

--- BST/kth-largest.cpp
+++ BST/kth-largest.cpp
@@ -25,29 +25,23 @@
     };
 
 ************************************************************/
-
 int solve(TreeNode<int>* root, int &i, int k) {
     
     if(root == NULL) {
         return -1;
     }
-
     // Reverse inorder traversal
     int right = solve(root->right, i, k);
 
     if(right != -1) {
         return right;
     }
-
     i++;
-
     if(i == k) {
         return root->data;
     }
-
     return solve(root->left, i, k);
 }
-
 int KthLargestNumber(TreeNode<int>* root, int k) 
 {
     int i = 0;
