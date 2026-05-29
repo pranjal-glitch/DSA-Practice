f8ccc425039f336bfb84e58b4f54fdcf2b1ac147
pranjal-glitch <pranjal29240705@gmail.com>
2026-05-29 23:42:55 +0530

    Update kth-smallest.cpp

--- BST/kth-smallest.cpp
+++ BST/kth-smallest.cpp
@@ -16,22 +16,18 @@ public:
         if(root == NULL) {
             return -1;
         }
-
         int left = solve(root->left, i, k);
 
         if(left != -1) {
             return left;
         }
-
         i++;
 
         if(i == k) {
             return root->val;
         }
-
         return solve(root->right, i, k);
     }
-
     int kthSmallest(TreeNode* root, int k) {
         int i = 0;
         int ans = solve(root, i, k);
