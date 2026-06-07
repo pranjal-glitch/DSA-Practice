7c45dd2288188e5cb0a2d78a3354228c1738a2f8
pranjal-glitch <pranjal29240705@gmail.com>
2026-06-07 23:55:39 +0530

    Fix formatting issues in pascals_triangle.cpp

--- Array/Arrays/pascals_triangle.cpp
+++ Array/Arrays/pascals_triangle.cpp
@@ -11,12 +11,9 @@ Approach:
 Time Complexity: O(n^2)
 Space Complexity: O(n^2)
 */
-
 class Solution {
 public:
 vector<vector<int>> generate(int numRows) {
-
-```
     vector<vector<int>> ans;
 
     for(int i = 0; i < numRows; i++){
